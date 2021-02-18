43e0a17caa7be978295b9b72deda2803b4168fc8
Omar Baldi <omarbaldidk@gmail.com>
2021-02-18 19:13:22 +0100

    Retrieved data from the API by making asynchronous http request and displayed movie poster for every single movie-row

--- src/components/movies.js
+++ src/components/movies.js
@@ -5,9 +5,9 @@ import MoviesRow from '../components/moviesRow/moviesRow';
 export default function Movies() {
     return (
         <>
-             <MoviesRow title={MoviesRequests.POPULAR_MOVIES.currentTitle} URL={MoviesRequests.POPULAR_MOVIES.currentURL} />
-             <MoviesRow title={MoviesRequests.TOP_RATED.currentTitle} URL={MoviesRequests.TOP_RATED.currentURL} />
-             <MoviesRow title={MoviesRequests.TRENDING_NOW.currentTitle} URL={MoviesRequests.TRENDING_NOW.currentURL} />
+             <MoviesRow title={MoviesRequests.POPULAR_MOVIES.currentTitle} url={MoviesRequests.POPULAR_MOVIES.currentURL} />
+             <MoviesRow title={MoviesRequests.TOP_RATED.currentTitle} url={MoviesRequests.TOP_RATED.currentURL} />
+             <MoviesRow title={MoviesRequests.TRENDING_NOW.currentTitle} url={MoviesRequests.TRENDING_NOW.currentURL} />
         </>
     )
 }
--- src/components/moviesRow/moviesRow.js
+++ src/components/moviesRow/moviesRow.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-//import Styles from './movies.module.scss';
+import Styles from './moviesRow.module.scss';
 
 
-export default function moviesRow({ title, URL }) {
+export default function MoviesRow({ title, url }) {
 
-    console.log(title, URL);
+    const [rowMovies, setRowMovies] = useState([]);
+
+    useEffect(async () => {
+        const response = await axios({
+            method: 'GET',
+            url
+        });
+        const { page, results, total_pages } = response.data;
+        setRowMovies(results);
+    }, []);
 
     return (
         <div>
-        
+            <h2 className={Styles.row__title}>{title}</h2>
+            <div className={Styles.movies__row}>
+                {
+                    rowMovies.map((movie, index) => {
+                        return (
+                            <img 
+                                key={index}
+                                className={Styles.movie}
+                                alt=""
+                                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
+                            />
+                        )
+                    })
+                }
+            </div>
+
         </div>
     )
 }
